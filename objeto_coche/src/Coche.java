7b2370f517339fad4aad98a0569277ba0acd1a42
harenmi <haren.m10@gmail.com>
2023-02-13 23:03:39 -0500

    ejercio 3 resuelto

---
+++ objeto_coche/src/Coche.java
@@ -0,0 +1,11 @@
+public class Coche {
+    public int numeroPuertas;
+
+    public int aumentarNumPuertas (int cantidadPuertas) {
+        numeroPuertas = numeroPuertas + cantidadPuertas;
+        return numeroPuertas;
+    }
+
+
+
+}
---
+++ objeto_coche/src/Main.java
@@ -0,0 +1,10 @@
+import java.io.Console;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("El numero de Puertas es:");
+        Coche miCoche = new Coche();
+        System.out.println(miCoche.aumentarNumPuertas(1));
+    }
+
+}
